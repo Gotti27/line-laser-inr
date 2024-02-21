5ed533ac2eec582ee0913ebe0c3498641f36f16e
Mario Gottardo <mario.gottardo27@gmail.com>
2024-02-21 19:10:23 +0100

    implicit neural representation model benchmark

---
+++ step-1/nn_benchmark.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+import numpy as np
+import rff
+import torch
+import torch.nn as nn
+
+
+class INR(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        sigma = 0.5
+        # self.B = torch.tensor([[random.gauss(0, sigma), random.gauss(0, sigma)] for _ in range(64)])
+        self.encoding = rff.layers.GaussianEncoding(sigma=sigma, input_size=2, encoded_size=64)
+        self.fc1 = nn.Linear(in_features=128, out_features=64)
+        self.fc2 = nn.Linear(in_features=64, out_features=32)
+        self.out = nn.Linear(in_features=32, out_features=1)
+
+    def forward(self, x):
+        x = self.encoding(x)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = nn.functional.tanh(self.out(x))
+
+        return x
+
+
+torch.manual_seed(41)
+model = INR()
+
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+model.load_state_dict(torch.load('model'))
+image = np.zeros((100, 100, 1), np.uint8)
+
+for i in range(100):
+    for j in range(100):
+        pred = model(torch.tensor([i, j]))
+        image[i, j] = (pred.item() + 1) * 127.5
+
+        cv.imshow("gear", image)
+        cv.waitKey(1)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
