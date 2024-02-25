efb1f9ed3fe20865e040447e13e2e00bae2307ef
Mario Gottardo <mario.gottardo27@gmail.com>
2024-02-25 14:40:14 +0100

    zoom image generated

--- step-1/nn_benchmark.py
+++ step-1/nn_benchmark.py
@@ -32,14 +32,21 @@ loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 model.load_state_dict(torch.load('model'))
 image = np.zeros((100, 100, 1), np.uint8)
+zoom = np.zeros((100, 100, 1), np.uint8)
 
 for i in range(100):
     for j in range(100):
-        pred = model(torch.tensor([i, j]))
-        image[i, j] = (pred.item() + 1) * 127.5
+        output = model(torch.tensor([[i / 2, (j + 100) / 2], [i, j]])).detach()
 
-        cv.imshow("gear", image)
+        zoom[i, j] = (output[0, 0] + 1) * 127.5
+        image[i, j] = (output[1, 0] + 1) * 127.5
+
+        cv.imshow("full", image)
+        cv.imshow("zoom", zoom)
         cv.waitKey(1)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+cv.imwrite('full.png', image)
+cv.imwrite('zoom.png', zoom)
