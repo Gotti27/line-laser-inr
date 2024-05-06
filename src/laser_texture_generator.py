2b4bb5e266e95c3e69493fdb6fa75cab1c319347
Mario Gottardo <mario.gottardo27@gmail.com>
2024-05-06 09:50:39 +0200

    laser line updated

--- src/laser_texture_generator.py
+++ src/laser_texture_generator.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 
 texture = np.zeros((1000, 1000, 3), np.uint8)
-cv.line(texture, (0, 500), (1000, 500), (0, 0, 255), 1)
+cv.line(texture, (500, 0), (500, 1000), (0, 0, 255), 1)
 
 cv.imshow("texture", texture)
 cv.imwrite("data/laser.bmp", texture)
